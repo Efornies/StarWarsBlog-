03653403f1a9c7319c4ef51bd0386f35a32ae7fc
Efornies <eleforbel@gmail.com>
2022-04-14 13:31:57 +0000

    cambios personajes y navbar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,40 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useContext, useEffect} from "react";
 import "../../styles/home.css";
+import {Context} from "../store/appContext";
+import { AiOutlineHeart } from '@react-icons/all-files/ai/AiOutlineHeart';
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+
+
+export const Home = () => {
+	const {store, actions} = useContext(Context);
+
+	useEffect(() => {
+		actions.getCharacters ()
+		}, [])
+
+
+	return (
+		<div>
+			<h1 className="title">Characters</h1>
+			<div className = "row">
+			{store.characters.map((character, i) => {
+				return <div key= {i} className="card text-white bg-dark mb-3" style={{width: "18rem"}}>
+				        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png" className="card-img-top" alt="..."/>
+						<div className="card-body">
+				  		<h5 className="card-title">{character.name}</h5>
+				  		<div className="card-text"> <p>Gender: {character.gender} </p> 
+						  <p>Hair color: {character.hair_color} </p>
+						  <p>Eye-color: {character.eye_color} </p></div>
+					
+
+				 		 <a href="#" className="btn btn-warning" style={{marginRight:"90px"}}>More Info</a>
+						  <a href="#" className="btn btn-warning"> <AiOutlineHeart /> </a>
+					
+						</div>
+			  		</div>
+			})}
+		</div>
+		</div>
+	)
+};
