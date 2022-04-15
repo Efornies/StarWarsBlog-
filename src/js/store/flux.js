9fd5a44e4348de0fcfb24b20103246d66e275601
Efornies <eleforbel@gmail.com>
2022-04-15 20:33:41 +0000

    intento 34567

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,9 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			characters: [],
-			gender: [],
-			haircolor: [],
-			eyecolor: [],
+			singlecharacter: [],
 			
 		},
 
@@ -14,7 +12,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const data = await response.json()
 				setStore({characters: data.results})
 				
+			},
+			
+			getSingleCharacter: async (key)=>{
+				const response = await  fetch(`https://www.swapi.tech/api/people/`+key)
+				const gettedcharacter = await response.json();
+				setStore({singlecharacter: gettedcharacter.result.properties})				
 			}
+
 
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,8 +4,6 @@ import {Context} from "../store/appContext";
 import { AiOutlineHeart } from '@react-icons/all-files/ai/AiOutlineHeart';
 
 
-
-
 export const Home = () => {
 	const {store, actions} = useContext(Context);
 
@@ -15,19 +13,19 @@ export const Home = () => {
 
 
 	return (
-		<div>
+
+		<div className = "container-fluid py-2">
 			<h1 className="title">Characters</h1>
-			<div className = "row">
-			{store.characters.map((character, i) => {
-				return <div key= {i} className="card text-white bg-dark mb-3" style={{width: "18rem"}}>
+			
+			<div className = "d-flex flex-row flex-nowrap" >
+			{store.characters.map((character) => {
+				return <div key= {character.uid} className="card text-white bg-dark mb-3" style={{width: "18rem"}}>
 				        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1200px-Star_Wars_Logo.svg.png" className="card-img-top" alt="..."/>
 						<div className="card-body">
 				  		<h5 className="card-title">{character.name}</h5>
 				  		<div className="card-text"> <p>Gender: {character.gender} </p> 
 						  <p>Hair color: {character.hair_color} </p>
 						  <p>Eye-color: {character.eye_color} </p></div>
-					
-
 				 		 <a href="#" className="btn btn-warning" style={{marginRight:"90px"}}>More Info</a>
 						  <a href="#" className="btn btn-warning"> <AiOutlineHeart /> </a>
 					
